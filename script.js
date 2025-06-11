3e1e77d02e9987d8f97eec83298986e46d0d5612
senior <isleepy72@gmail.com>
2025-06-11 19:01:00 +0400

    First Commit

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleText(){
-    const textBlock = document.getElementById("hiddenText");
-
-    if(textBlock.style.display === "none"){
-        textBlock.style.display = "block";
-    }   else {
-        textBlock.style.display = "none";
-    }
-}
